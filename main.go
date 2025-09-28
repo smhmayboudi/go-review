fc14e279543ccfe5abf63589008a996badc2dfc6
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-09-28 08:14:52 +0330

    feat: add user struct

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name string
+	Age  int
+}
+
+func (u User) Greet() string {
+	return fmt.Sprintf("Hello I am %s, and %d years old.", u.Name, u.Age)
+}
+
 func main() {
-	fmt.Println("hello world!")
+	user := User{Name: "Hossein Mayboudi", Age: 45}
+	fmt.Println(user.Greet())
 }
